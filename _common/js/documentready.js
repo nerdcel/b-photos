19e6bca0c900a393b590df9249af381e6bd57c99
Marcel Hieke <marcel.hieke@googlemail.com>
2013-06-23 09:44:59 +0200

    paralax

--- _common/js/documentready.js
+++ _common/js/documentready.js
@@ -4,19 +4,35 @@ $(function(){
 	
 	$(window).scroll(function(e)
 	{
-		if( parseInt( $(document).scrollTop()) > ( parseInt($('#gallery').height()) / 2 ) )
+		/* top - 50 */
+		if( parseInt( $(document).scrollTop(),10) > ( parseInt($('#gallery').height(),10) - 50 ) )
 		{
-			$('.moduleNavigationMain').addClass('fixedPosition');
+			$('body').addClass('fixedPosition');
 		}
 		else
 		{
-			$('.moduleNavigationMain').removeClass('fixedPosition');
+			$('body').removeClass('fixedPosition');
 		}
 		
-		if( parseInt( $(document).scrollTop()) > 175 )
+		/* bottom + 175 */		
+		if( parseInt( $(document).scrollTop(), 10) > 175 )
 			$('.moduleImageSlider nav').hide();
 		else
 			$('.moduleImageSlider nav').show();
+			
+		/* top - 175 */
+		if( parseInt( $(document).scrollTop(), 10) > ( parseInt($('#gallery').height(),10) - 175 ) )
+			$('#logo').addClass("yellow");
+		else
+			$('#logo').removeClass("yellow");
+			
+		
+		/* 50% */
+		if( parseInt( $(document).scrollTop(), 10) > (( parseInt($('#gallery').height(),10) / 2 ) - 25 ) )
+			$('.moduleImageSlider').addClass("hideHeader");
+		else
+			$('.moduleImageSlider').removeClass("hideHeader");
+		
 		
 	});
 	
